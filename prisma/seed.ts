108fff2a47d77b524e8ebd0969090128b70a96f4
Abdule Yousuf Hashi <114812664+abduleyousuf08@users.noreply.github.com>
2024-05-20 10:33:28 +0300

    Finished Dashboard

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,46 +1,47 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from '@prisma/client';
 
 import {
-  brands,
-  services,
-  categories,
-  series,
-  model,
-  modelCategory,
-} from "./seededitems";
+   brands,
+   categories,
+   model,
+   modelCategory,
+   series,
+   services,
+} from './seededitems';
+
 const prisma = new PrismaClient();
 
 async function main() {
-  await prisma.service.createMany({
-    data: services,
-  });
+   await prisma.service.createMany({
+      data: services,
+   });
 
-  await prisma.brand.createMany({
-    data: brands,
-  });
+   await prisma.brand.createMany({
+      data: brands,
+   });
 
-  await prisma.category.createMany({
-    data: categories,
-  });
+   await prisma.category.createMany({
+      data: categories,
+   });
 
-  await prisma.series.createMany({
-    data: series,
-  });
+   await prisma.series.createMany({
+      data: series,
+   });
 
-  await prisma.model.createMany({
-    data: model,
-  });
+   await prisma.model.createMany({
+      data: model,
+   });
 
-  await prisma.modelCategory.createMany({
-    data: modelCategory,
-  });
+   await prisma.modelCategory.createMany({
+      data: modelCategory,
+   });
 }
 
 main()
-  .catch((error) => {
-    console.log(error);
-    process.exit(1);
-  })
-  .finally(() => {
-    prisma.$disconnect();
-  });
+   .catch((error) => {
+      console.log(error);
+      process.exit(1);
+   })
+   .finally(() => {
+      prisma.$disconnect();
+   });
