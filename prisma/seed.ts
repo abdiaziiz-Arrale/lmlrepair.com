70158859d175de3c84860003e4dba4d97cd4ab7d
abdelaziz-allem <abdelaziz.allem@gmail.com>
2024-05-29 21:18:18 +0300

    restoring removed seeded items

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,72 +1,105 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from "@prisma/client";
 
 import {
-   ItemsCategory,
-   ItemsSubCategory,
-   Vendor,
-   Location,
-   InventoryItem,
-   Variation,
-   StockReturn,
-   InternalTransfer,
-   Sale,
-   InventoryAge,
-   LowStockAlert,
-} from './inventoryItems';
+  ItemsCategory,
+  ItemsSubCategory,
+  Vendor,
+  Location,
+  InventoryItem,
+  Variation,
+  StockReturn,
+  InternalTransfer,
+  Sale,
+  InventoryAge,
+  LowStockAlert,
+} from "./inventoryItems";
+
+import {
+  brands,
+  categories,
+  model,
+  modelCategory,
+  series,
+  services,
+} from "./serviceItems";
 
 const prisma = new PrismaClient();
 
 async function main() {
-   await prisma.itemsCategory.createMany({
-      data: ItemsCategory,
-   });
+  await prisma.brand.createMany({
+    data: brands,
+  });
 
-   await prisma.itemsSubCategory.createMany({
-      data: ItemsSubCategory,
-   });
+  await prisma.service.createMany({
+    data: services,
+  });
 
-   await prisma.vendor.createMany({
-      data: Vendor,
-   });
+  await prisma.series.createMany({
+    data: series,
+  });
 
-   await prisma.location.createMany({
-      data: Location,
-   });
+  await prisma.modelCategory.createMany({
+    data: modelCategory,
+  });
 
-   await prisma.inventoryItem.createMany({
-      data: InventoryItem,
-   });
+  await prisma.model.createMany({
+    data: model,
+  });
 
-   await prisma.variation.createMany({
-      data: Variation,
-   });
+  await prisma.category.createMany({
+    data: categories,
+  });
 
-   await prisma.stockReturn.createMany({
-      data: StockReturn,
-   });
+  await prisma.itemsCategory.createMany({
+    data: ItemsCategory,
+  });
 
-   await prisma.internalTransfer.createMany({
-      data: InternalTransfer,
-   });
+  await prisma.itemsSubCategory.createMany({
+    data: ItemsSubCategory,
+  });
 
-   await prisma.sale.createMany({
-      data: Sale,
-   });
+  await prisma.vendor.createMany({
+    data: Vendor,
+  });
 
-   await prisma.inventoryAge.createMany({
-      data: InventoryAge,
-   });
+  await prisma.location.createMany({
+    data: Location,
+  });
 
-   await prisma.lowStockAlert.createMany({
-      data: LowStockAlert,
-   });
+  await prisma.inventoryItem.createMany({
+    data: InventoryItem,
+  });
+
+  await prisma.variation.createMany({
+    data: Variation,
+  });
+
+  await prisma.stockReturn.createMany({
+    data: StockReturn,
+  });
+
+  await prisma.internalTransfer.createMany({
+    data: InternalTransfer,
+  });
+
+  await prisma.sale.createMany({
+    data: Sale,
+  });
+
+  await prisma.inventoryAge.createMany({
+    data: InventoryAge,
+  });
+
+  await prisma.lowStockAlert.createMany({
+    data: LowStockAlert,
+  });
 }
 
 main()
-   .catch((error) => {
-      console.error(error);
-      process.exit(1);
-   })
-   .finally(async () => {
-      await prisma.$disconnect();
-   });
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
